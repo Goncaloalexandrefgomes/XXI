b68d2b2906c98ade1e7dcb1af55365ad9c006093
goncaloalexandrefgomes <goncalo.gomes1412003@gmail.com>
2021-02-16 17:33:06 +0000

    Initial commit

---
+++ db.py
@@ -0,0 +1,6 @@
+class Database:
+    def __init__(self):
+        self.Host = 'ec2-79-125-64-18.eu-west-1.compute.amazonaws.com'
+        self.Database = 'ddl4kr8rvkoscg'
+        self.User = 'xmvkgmqtcfbplx'
+        self.Password = '80b8fb5bd23fdcf122a4270230b9daa963e6398c70eb13dc4e17d3b31b4b29a4'
